103619469d57f856e623eb6fa76e26720e88e713
Tobby004 <tobbytee22@gmail.com>
2023-10-14 16:16:41 +0000

    first commit

---
+++ .history/nav_20230429063917.js
@@ -0,0 +1,8 @@
+ffunction myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
---
+++ .history/nav_20230430101107.js
@@ -0,0 +1,3 @@
+function hamburger(){
+   menu.classlist.toggle("open");
+}
